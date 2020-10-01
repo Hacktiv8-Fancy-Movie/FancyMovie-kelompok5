28176f66400fe15a35bd09a3d19a7f53830d77d4
ikhsangama <ikhsangama@gmail.com>
2020-10-02 01:00:03 +0700

    err handler

--- server/controllers/CalenderController.js
+++ server/controllers/CalenderController.js
@@ -2,9 +2,10 @@ const axios = require('axios')
 
 class CalenderController {
     static getHoliday(req, res) {
+        let key = "73e908957f6078d58186d94fe7e98c2c7062fee9"
         axios({
             method: 'get',
-            url: `https://calendarific.com/api/v2/holidays?api_key=${process.env.calendarificAPIkey}&country=ID&year=2020`
+            url: `https://calendarific.com/api/v2/holidays?api_key=${key}&country=ID&year=2020`
         })
         .then(response => {
             let holidays = response.data.response.holidays
