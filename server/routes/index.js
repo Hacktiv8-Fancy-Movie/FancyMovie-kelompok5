5f7f25738cdc71b2ce3a663fd2847aa651aaac1e
nasruljims <nasrul.gat@gmail.com>
2020-10-01 20:20:57 +0700

    Calender route + controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 const users = require("./route-users")
+const routeCalender = require('./calender')
 
 router.get('/', (req, res) => {
   res.json({msg: "Hello from index"})
 })
 
 router.use('/users', users)
+router.use('/calender', routeCalender)
 
 module.exports = router
