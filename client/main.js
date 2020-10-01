e8ad16ce7c6561a9f69d134b569557c7e5ba1c6c
basiliuswicaksono <basilius.wicaksono@gmail.com>
2020-10-01 23:46:47 +0800

    edit routes movie, tampilan movie, main.js movie

--- client/main.js
+++ client/main.js
@@ -2,7 +2,8 @@ let baseUrl = "http://localhost:3000"
 let tempId = null
 
 $(document).ready(function() {
-    checkLogin()
+    // checkLogin()
+    toMoviesView()
 });
 
 // kalau ada tambahan list view ditambah disini, fungsi untuk menentukan mana yang mau di view
@@ -19,10 +20,12 @@ function pageView(el){
 function checkLogin(){
   // jika berhasil login
   if(localStorage.token){
+
     pageView('#movies-view')
   }
   // jika tidak berhasil login
   else{
+
     pageView('#login-view')
   }
 }
@@ -37,7 +40,36 @@ function toLoginView(event){
 
 function toMoviesView(){
   // 
+  $.ajax({
+    url: `${baseUrl}/movies`,
+    method: 'get',
+    headers: {
+        token: localStorage.token
+    }
+  })
+    .done(movies => {
+        console.log(movies, '<<< data movies')
+        $('#container-movies').empty()
+        let i = 0
+        movies.forEach(movie => {
+            i++
+            $('#container-movies').append(`
+            <tr >
+            <th scope="row">${i}</th>
+            <td>${movie.title}</td>
+            <td>${movie.popularity}</td>
+            <td>${movie.overview}</td>
+            <td>  <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="movie poster"> </td>
+            </tr>
+            `)
+        });
+    })
+    .fail(err =>{
+        console.log(err.responseJSON.errors)
+    })
 }
+
+
 
 function toMusicsView(){
   // 
--- server/routes/route-movie.js
+++ server/routes/route-movie.js
@@ -9,8 +9,10 @@ router.get("/", (req, res, next)=>{
         url: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=4f8d0668dd2725012395df54297fb76a'
     })
     .then(movies=>{
+        let movieList = movies.data.results
+        console.log(movieList)
         res.status(200).json(
-            movies.data.results
+            movieList
         )
     })
     .catch(err=> next(err))
