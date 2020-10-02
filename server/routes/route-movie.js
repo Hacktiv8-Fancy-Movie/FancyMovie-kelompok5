35ea592ffbb2009a87b5ec22fccffdde2a9ff05a
basiliuswicaksono <basilius.wicaksono@gmail.com>
2020-10-02 11:52:30 +0800

    update minor

--- server/routes/route-movie.js
+++ server/routes/route-movie.js
@@ -3,14 +3,12 @@ const axios = require('axios')
 
 //api themoviedb
 router.get("/", (req, res, next)=>{
-    console.log('tes')
     axios({
         method: 'GET',
         url: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=4f8d0668dd2725012395df54297fb76a'
     })
     .then(movies=>{
         let movieList = movies.data.results
-        // console.log(movieList)
         res.status(200).json(
             movieList
         )
